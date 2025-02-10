39ef3dad8f395de8374bb530314cb24fc5044abf
maxlayn1 <maxlayn1@gmail.com>
2025-02-10 09:03:40 -0600

    Good structure for the "interpreter" python file i think

--- interface.py
+++ interface.py
@@ -3,3 +3,23 @@
 #read from text file 3 prompts
 #pass these 3 prompts to model
 #copy answers and store in another txt file
+
+def main(): #call functions from here 
+    print('main')
+
+def choose_LLM(): #ask user whether they want to use Gemma2-2b, or Phi3
+    print('which LLM would you like to use?')
+
+def read_prompt_file(): #read text file containing prompts
+    print('read from prompt file')
+
+def pass_prompt_to_LLM(): #ask LLM to interpret prompt sufficiently
+    print('passing prompt to LLM')
+
+def read_LLM_response(): #interpret LLM's response
+    print('reading response from LLM')
+
+def store_LLM_response(): #store LLM's response in seperate txt file
+    print('storing LLM response in txt file')
+
+main()
