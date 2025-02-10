1409f8b92a47635ce687e2db9f1c22c55b842ff1
maxlayn1 <maxlayn1@gmail.com>
2025-02-10 14:15:12 -0600

    Successfully storing LLM response in txt file now, just need it to iterate thru each line of prompt file.

--- interface.py
+++ interface.py
@@ -1,12 +1,15 @@
-import subprocess #allows python to input terminal commands
+import subprocess                                                       #allows python to input terminal commands
+import time                                                             #for listing time of LLM response in reponses txt file
 
-def main(): #call functions from here 
+def main():           
     LLM_name = choose_LLM()
     prompt_list = read_prompt_file()
     response = pass_prompt_to_LLM(prompt_list, LLM_name)
-    print(response)
+    clean_response = read_LLM_response(response)
+    store_LLM_response(clean_response)
+    print('LLM RESPONSES STORED')
 
-def choose_LLM(): #ask user whether they want to use Gemma2-2b, or Phi3
+def choose_LLM():                                                       #ask user whether they want to use Gemma2-2b, or Phi3
     LLM = input('Choose an LLM: 1. Gemma2-2b   2. Phi3\n')
     if LLM == '1':
         #print('Using Gemma2-2b')
@@ -15,18 +18,15 @@ def choose_LLM(): #ask user whether they want to use Gemma2-2b, or Phi3
         #print('Using Phi3')
         return 'phi3:latest'
     else:
-        print('Error, invalid input')
-        exit
+        raise SystemExit(1)                                             #crash program if unexpected input for now
 
-def read_prompt_file(): #read text file containing prompts, returning all the prompts in a list
+def read_prompt_file():                                                 #read text file containing prompts, returning all the prompts in a list
     with open('prompts.txt', 'r') as file:
         lines = file.readlines()
-        lines = [empty for empty in lines if empty.strip()] #trims empty lines from txt prompt file
-    #for line in lines:
-        #print(line)
+        lines = [empty for empty in lines if empty.strip()]             #trims empty lines from txt prompt file
     return lines
 
-def pass_prompt_to_LLM(prompt_list, LLM_name): #ask LLM to interpret prompt thru user's terminal
+def pass_prompt_to_LLM(prompt_list, LLM_name):                          #ask LLM to interpret prompt thru user's terminal
     try:
         LLM_PROMPT = 'Read the following news headline and determine if it is positive, negative, or neutral. When responding use only these three words, and respond with only one word.'
         command = "ollama run " + LLM_name + " \"" + LLM_PROMPT + prompt_list[0] + "\"" + "\n"
@@ -35,10 +35,12 @@ def pass_prompt_to_LLM(prompt_list, LLM_name): #ask LLM to interpret prompt thru
     except subprocess.CalledProcessError as e:
         return f"Error: {e.stderr}"
 
-def read_LLM_response(): #interpret LLM's response
-    print('reading response from LLM')
+def read_LLM_response(unclean_response):                                #interpret LLM's response
+    return unclean_response.strip().lower()
 
-def store_LLM_response(): #store LLM's response in seperate txt file
-    print('storing LLM response in txt file')
+def store_LLM_response(clean_response):                                 #store LLM's response in seperate txt file                      
+    file = open('responses.txt', 'w')
+    file.write(f'-------LLM RESPONSE AT {time.strftime("%H:%M:%S")}------\n')
+    file.write(f'{clean_response}\n')
 
 main()
