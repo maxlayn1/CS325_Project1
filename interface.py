09859972f12a1d0a0c06314846ac586aaed5903a
maxlayn1 <maxlayn1@gmail.com>
2025-02-10 10:40:43 -0600

    Trying to have the python script interact with the LLM thru the terminal. Made good progress on this.

--- interface.py
+++ interface.py
@@ -1,32 +1,39 @@
-#idk yet // dont forget to add yaml file too
-
-#read from text file 3 prompts
-#pass these 3 prompts to model
-#copy answers and store in another txt file
+import subprocess #allows python to input terminal commands
 
 def main(): #call functions from here 
-    print('main')
-    read_prompt_file()
+    LLM_name = choose_LLM()
+    prompt_list = read_prompt_file()
+    response = pass_prompt_to_LLM(prompt_list, LLM_name)
+    print(response)
 
 def choose_LLM(): #ask user whether they want to use Gemma2-2b, or Phi3
     LLM = input('Choose and LLM: 1. Gemma2-2b   2. Phi3')
     if LLM == '1':
         print('Using Gemma2-2b')
+        return 'Gemma2-2b'
     elif LLM == '2':
         print('Using Phi3')
+        return 'Phi3'
     else:
         print('Error, invalid input')
         exit
 
-def read_prompt_file(): #read text file containing prompts, adding all the lines into a list
+def read_prompt_file(): #read text file containing prompts, returning all the prompts in a list
     with open('prompts.txt', 'r') as file:
         lines = file.readlines()
         lines = [empty for empty in lines if empty.strip()] #trims empty lines from txt prompt file
-    for line in lines:
-        print(line)
+    #for line in lines:
+        #print(line)
+    return lines
 
-def pass_prompt_to_LLM(): #ask LLM to interpret prompt sufficiently
-    print('passing prompt to LLM')
+def pass_prompt_to_LLM(prompt_list, LLM_name): #ask LLM to interpret prompt thru user's terminal
+    try:
+        LLM_PROMPT = 'Read the following news headline and determine if it is positive, negative, or neutral. When responding use only these three words, and respond with only one word.'
+        command = "ollama run" + "\"" + LLM_name + LLM_PROMPT + prompt_list[0] + "\n"
+        result = subprocess.run(command, shell=True, text=True, capture_output=True, check=True)
+        return result.stdout
+    except subprocess.CalledProcessError as e:
+        return f"Error: {e.stderr}"
 
 def read_LLM_response(): #interpret LLM's response
     print('reading response from LLM')
