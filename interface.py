0f3c9e2be379dac4385b83756e1941929d12c5c2
maxlayn1 <maxlayn1@gmail.com>
2025-02-10 12:58:13 -0600

    Got the prompt to work in terminal. So far, the LLM responds with "positive", "negative", "neutral" appropriately.

--- interface.py
+++ interface.py
@@ -9,11 +9,11 @@ def main(): #call functions from here
 def choose_LLM(): #ask user whether they want to use Gemma2-2b, or Phi3
     LLM = input('Choose an LLM: 1. Gemma2-2b   2. Phi3\n')
     if LLM == '1':
-        print('Using Gemma2-2b')
-        return 'Gemma2-2b'
+        #print('Using Gemma2-2b')
+        return 'gemma2:2b'
     elif LLM == '2':
-        print('Using Phi3')
-        return 'Phi3'
+        #print('Using Phi3')
+        return 'phi3:latest'
     else:
         print('Error, invalid input')
         exit
@@ -29,7 +29,7 @@ def read_prompt_file(): #read text file containing prompts, returning all the pr
 def pass_prompt_to_LLM(prompt_list, LLM_name): #ask LLM to interpret prompt thru user's terminal
     try:
         LLM_PROMPT = 'Read the following news headline and determine if it is positive, negative, or neutral. When responding use only these three words, and respond with only one word.'
-        command = "ollama run" + "\"" + LLM_name + LLM_PROMPT + prompt_list[0] + "\n"
+        command = "ollama run " + LLM_name + " \"" + LLM_PROMPT + prompt_list[0] + "\"" + "\n"
         result = subprocess.run(command, shell=True, text=True, capture_output=True, check=True)
         return result.stdout
     except subprocess.CalledProcessError as e:
