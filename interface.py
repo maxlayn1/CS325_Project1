d762c12f310b81b5dad7d63d613e8a783957f0ab
maxlayn1 <maxlayn1@gmail.com>
2025-02-10 14:26:39 -0600

    LLM is now iteratively given every prompt in the prompts.txt file, and each response is then stored in responses.txt

--- interface.py
+++ interface.py
@@ -4,18 +4,17 @@ import time                                                             #for lis
 def main():           
     LLM_name = choose_LLM()
     prompt_list = read_prompt_file()
-    response = pass_prompt_to_LLM(prompt_list, LLM_name)
-    clean_response = read_LLM_response(response)
-    store_LLM_response(clean_response)
+    clean_response_list = pass_prompt_to_LLM(prompt_list, LLM_name)
+    store_LLM_response(clean_response_list)
     print('LLM RESPONSES STORED')
 
 def choose_LLM():                                                       #ask user whether they want to use Gemma2-2b, or Phi3
     LLM = input('Choose an LLM: 1. Gemma2-2b   2. Phi3\n')
     if LLM == '1':
-        #print('Using Gemma2-2b')
+        print('Using Gemma2-2b')
         return 'gemma2:2b'
     elif LLM == '2':
-        #print('Using Phi3')
+        print('Using Phi3')
         return 'phi3:latest'
     else:
         raise SystemExit(1)                                             #crash program if unexpected input for now
@@ -28,19 +27,23 @@ def read_prompt_file():                                                 #read te
 
 def pass_prompt_to_LLM(prompt_list, LLM_name):                          #ask LLM to interpret prompt thru user's terminal
     try:
-        LLM_PROMPT = 'Read the following news headline and determine if it is positive, negative, or neutral. When responding use only these three words, and respond with only one word.'
-        command = "ollama run " + LLM_name + " \"" + LLM_PROMPT + prompt_list[0] + "\"" + "\n"
-        result = subprocess.run(command, shell=True, text=True, capture_output=True, check=True)
-        return result.stdout
+        response_list = []
+        for prompt in prompt_list:
+            LLM_PROMPT = 'Read the following news headline and determine if it is positive, negative, or neutral. When responding use only these three words, and respond with only one word.'
+            command = "ollama run " + LLM_name + " \"" + LLM_PROMPT + prompt + "\"" + "\n"
+            result = subprocess.run(command, shell=True, text=True, capture_output=True, check=True)
+            response_list.append(read_LLM_response(result.stdout))      #adds response to a list after cleaning it
+        return response_list
     except subprocess.CalledProcessError as e:
         return f"Error: {e.stderr}"
 
 def read_LLM_response(unclean_response):                                #interpret LLM's response
     return unclean_response.strip().lower()
 
-def store_LLM_response(clean_response):                                 #store LLM's response in seperate txt file                      
+def store_LLM_response(clean_response_list):                                 #store LLM's response in seperate txt file                      
     file = open('responses.txt', 'w')
     file.write(f'-------LLM RESPONSE AT {time.strftime("%H:%M:%S")}------\n')
-    file.write(f'{clean_response}\n')
+    for response in clean_response_list:
+        file.write(f'{response}\n')
 
 main()
