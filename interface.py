dacd14d89a5e6efb47481c08d2159e2ff11ccd07
maxlayn1 <maxlayn1@gmail.com>
2025-02-12 11:25:32 -0600

    Made interface.py's workflow more clear with comments and improved prompt

--- interface.py
+++ interface.py
@@ -2,35 +2,35 @@ import subprocess                                                           #all
 import time                                                                 #allows for listing time of LLM response in reponses.txt file
 
 def main():           
-    LLM_name = choose_LLM()
-    prompt_list = read_prompt_file()
-    clean_response_list = pass_prompt_to_LLM(prompt_list, LLM_name)
-    store_LLM_response(clean_response_list)
-    print('LLM RESPONSES STORED')
+    LLM_name = choose_LLM()                                                 #user chooses which LLM they want to use
+    prompt_list = read_prompt_file()                                        #parse prompt file
+    clean_response_list = pass_prompt_to_LLM(prompt_list, LLM_name)         #prompt is passed to LLM and result is stored
+    store_LLM_response(clean_response_list)                                 #LLM responses stored in responses.txt
+    print('LLM RESPONSES STORED')                                           #indicate to the user that the script is done
 
 def choose_LLM():                                                           #ask whether user wants to use Gemma2-2b or Phi3
     LLM = input('Choose an LLM: 1. Gemma2-2b   2. Phi3\n')
     if LLM == '1':
         print('Using Gemma2-2b')
-        return 'gemma2:2b'
+        return 'gemma2:2b'                                                  #user choose gemma2-2b
     elif LLM == '2':
         print('Using Phi3')
-        return 'phi3:latest'
+        return 'phi3:latest'                                                #user choose phi3
     else:
         raise SystemExit(1)                                                 #crash program if unexpected input
 
 def read_prompt_file():                                                     #read prompts.txt, returning all the prompts in a list
     with open('prompts.txt', 'r') as file:
         lines = file.readlines()
-        lines = [empty for empty in lines if empty.strip()]
-        lines.pop()                 #trims empty lines from prompts.txt
+        lines = [empty for empty in lines if empty.strip()]                 #trims empty lines from prompts.txt
+        lines.pop()                                                         #excludes comment at the top of prompts.txt from evaluation
     return lines
 
 def pass_prompt_to_LLM(prompt_list, LLM_name):                              #ask LLM to interpret prompt thru user's terminal
     try:
         response_list = []
         for prompt in prompt_list:
-            LLM_PROMPT = 'Read the following news headline and determine if it is positive, negative, or neutral. When responding use only these three words, and respond with only one word. Do NOT give more than one word.'
+            LLM_PROMPT = 'Read the following news headline and determine if it is positive, negative, or neutral. When responding use only these three words, and respond with only one word. Do NOT respond futher than the one word.'
             command = "ollama run " + LLM_name + " \"" + LLM_PROMPT + prompt + "\"" + "\n"
             result = subprocess.run(command, shell=True, text=True, capture_output=True, check=True)
             response_list.append(clean_LLM_response(result.stdout))          #adds response to a list after cleaning it
@@ -42,7 +42,7 @@ def clean_LLM_response(unclean_response):                                    #cl
     return unclean_response.strip().lower()
 
 def store_LLM_response(clean_response_list):                                #store LLM's responses in responses.txt                 
-    file = open('responses.txt', 'w')
+    file = open('responses.txt', 'a')
     file.write(f'-------LLM RESPONSE AT {time.strftime("%H:%M:%S")}------\n')
     for response in clean_response_list:
         file.write(f'{response}\n')
