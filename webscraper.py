f98d25db17e8d07e6eb80b8a357f4cae0d28634d
maxlayn1 <maxlayn1@gmail.com>
2025-04-09 11:11:49 -0500

    Added comments and removed test print statements

--- webscraper.py
+++ webscraper.py
@@ -11,15 +11,15 @@ def main():
 def read_url_file():
     with open('input_urls.txt', 'r') as file:
         lines = file.readlines()
-        lines = [line.strip() for line in lines if line.strip() and not line.strip().startswith('#')]
+        lines = [line.strip() for line in lines if line.strip() and not line.strip().startswith('#')] # skip empty lines and lines starting with '#'
     return lines
         
 def scrape_urls(urls, site_to_scrape):
     all_headlines = []
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
-    if (site_to_scrape == '1'):
+    if (site_to_scrape == '1'):                                                         # Chicago Tribune
         try:
-            response = requests.get(urls[0], headers=headers)                       # get the HTML content
+            response = requests.get(urls[0], headers=headers)                           # get the HTML content
             response.raise_for_status()
             soup = bs4.BeautifulSoup(response.text, 'html.parser')
             headlines = soup.find_all('span', class_ = 'dfm-title')  
@@ -27,18 +27,16 @@ def scrape_urls(urls, site_to_scrape):
         except Exception as e:
             print(f"Error scraping {urls[0]}: {e}")
         return all_headlines
-    elif (site_to_scrape == '2'):
+    elif (site_to_scrape == '2'):                                                       # U.S. News
         try:
             response = requests.get(urls[1], headers=headers)
             response.raise_for_status()
             soup = bs4.BeautifulSoup(response.text, 'html.parser')
-            
-            #print(soup.prettify())
             h3s = soup.find_all('h3', class_='story-headline')
             for h3 in h3s:
-                    anchor = h3.find('a')
+                    anchor = h3.find('a')                                               # Find the anchor tag within the h3
                     if anchor:
-                            all_headlines.append(anchor.text.strip())
+                            all_headlines.append(anchor.text.strip())                   # Add the headline to the list
         except Exception as e:
             print(f"Error scraping {urls[1]}: {e}")
         return all_headlines
@@ -49,7 +47,7 @@ def scrape_urls(urls, site_to_scrape):
 
 def store_scraped_headlines(headlines):
     file = open('scraped_headlines.txt', 'a')
-    file.write(f'-------HEADLINES SCRAPED AT {time.strftime("%H:%M:%S")}------\n')
+    file.write(f'-------HEADLINES SCRAPED AT {time.strftime("%H:%M:%S")}------\n')      # store the time of scraping
     for headline in headlines:
         file.write(f'{headline}\n')
     file.write('\n')
