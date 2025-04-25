4e56e5cff70f35b326d059360a1c2fbd752eb4ec
maxlayn1 <maxlayn1@gmail.com>
2025-04-25 12:32:42 -0500

    progress on refactor to OOP

--- interface.py
+++ interface.py
@@ -9,58 +9,74 @@ class LLM(Protocol):
     def respond(self):
         pass
     
-class HeadlineLLM:
-    def query(self):
-        pass
-    def respond (self):
-        pass
-    def clean_response(self):
-        pass
-    def store_response(self):
-        pass
-    
 class FileHandler(Protocol):
     def read(self):
         pass
     def write(self):
         pass
+    
+class HeadlineLLM:
+    def query(self, prompt_list, LLM_name):
+        try:
+            response_list = []
+            for prompt in prompt_list:
+                LLM_PROMPT = 'Read the following news headline and determine if it is positive, negative, or neutral. When responding use only one of these three words, and respond with only one word. Do NOT respond further than the one word:'
+                command = "ollama run " + LLM_name + " \"" + LLM_PROMPT + prompt + "\"" + "\n"
+                result = subprocess.run(command, shell=True, text=True, capture_output=True, check=True) #runs command in terminal
+            response_list.append(clean_LLM_response(result.stdout))         #adds response to a list after cleaning it
+            return response_list                                                #returns list of LLM's responses
+        except subprocess.CalledProcessError as e:
+            return f"Error: {e.stderr}"
+    def respond (self):
+        pass
+    def clean_response(self, unclean_response):
+        return unclean_response.strip().lower()
+    def store_response(self, clean_response_list):                              #store LLM's responses in responses.txt
+        file = open('responses.txt', 'a')
+        file.write(f'#-------LLM RESPONSE AT {time.strftime("%H:%M:%S")}------\n')
+        for response in clean_response_list:
+            file.write(f'{response}\n')
+    def choose_LLM(self):
+        LLM = input('Choose an LLM: 1. Gemma2-2b   2. llama3.2\n')
+        if LLM == '1':
+            print('Using Gemma2-2b')
+            return 'gemma2:2b'                                                  #user choose gemma2-2b
+        elif LLM == '2':
+            print('Using llama3.2')
+            return 'llama3.2:3b'                                                #user choose llama3.2
+        else:
+            raise SystemExit(1)                                                 #crash program if unexpected input
 
 class HeadlineResponseHandler:
     def read(self):
-        pass
+        with open('scraped_headlines.txt', 'r') as file:
+            lines = file.readlines()
+            lines = [line for line in lines if line.strip() and not line.strip().startswith('#')]  # skip empty lines and lines starting with '#'   
+            return lines
     def write(self):
-        pass
+        file = open('responses.txt', 'a')
+        file.write(f'#-------LLM RESPONSE AT {time.strftime("%H:%M:%S")}------\n')
+        for response in clean_response_list:
+            file.write(f'{response}\n')
     def clean(self):
         pass
 
 def main():           
-    webscraper.main()                                                       #run webscraper.py to scrape headlines
-    LLM_name = choose_LLM()                                                 #user chooses which LLM they want to use
-    prompt_list = read_prompt_file()     
-    clean_response_list = pass_prompt_to_LLM(prompt_list, LLM_name)         #prompt is passed to LLM and result is stored
-    store_LLM_response(clean_response_list)                                 #LLM responses stored in responses.txt
-    print('LLM RESPONSES STORED')                                           #indicate to the user that the script is done
-    
-    # url_handler = URLHeadlineHandler()
-    # scraper = HeadlineScraper()
-
     # urls = url_handler.read()
     # clean_urls = url_handler.clean(urls)
     # site_to_scrape = scraper.choose_site_to_scrape()
     # soup = scraper.connect_to_site(site_to_scrape, clean_urls)
     # headlines = scraper.scrape(site_to_scrape, soup)
     # url_handler.write(headlines)
-
-def choose_LLM():                                                           #ask whether user wants to use Gemma2-2b or llama3.2
-    LLM = input('Choose an LLM: 1. Gemma2-2b   2. llama3.2\n')
-    if LLM == '1':
-        print('Using Gemma2-2b')
-        return 'gemma2:2b'                                                  #user choose gemma2-2b
-    elif LLM == '2':
-        print('Using llama3.2')
-        return 'llama3.2:3b'                                                #user choose llama3.2
-    else:
-        raise SystemExit(1)                                                 #crash program if unexpected input
+    
+    # LLM_name = choose_LLM()                                                 #user chooses which LLM they want to use
+    # prompt_list = read_prompt_file()     
+    # clean_response_list = pass_prompt_to_LLM(prompt_list, LLM_name)         #prompt is passed to LLM and result is stored
+    # store_LLM_response(clean_response_list)                                 #LLM responses stored in responses.txt
+    # print('LLM RESPONSES STORED')                                           #indicate to the user that the script is done
+    
+    # url_handler = URLHeadlineHandler()
+    # scraper = HeadlineScraper()
 
 def read_prompt_file():                                                     #read prompts.txt, returning all the prompts in a list
     with open('scraped_headlines.txt', 'r') as file:
